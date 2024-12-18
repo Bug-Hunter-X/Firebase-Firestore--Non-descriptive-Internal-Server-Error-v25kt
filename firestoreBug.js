c3ef90c6ba7ee7816096d86744848688d6cdac24
BugHunter-X <askzenai@gmail.com>
2024-12-18 13:20:04 +0100

    Add firestoreBug.js file

---
+++ firestoreBug.js
@@ -0,0 +1,1 @@
+The Firebase SDK sometimes throws an error that is not very descriptive.  For example, when attempting to write data to a Firestore database, you might receive a generic 'Internal Server Error'.  Debugging this can be challenging without more specific information from the SDK.
